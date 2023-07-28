5ecaaae672a5a46487b73053429239e411e87946
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-28 23:45:21 +0900

    chore(utils): add utilities

---
+++ utils.ts
@@ -0,0 +1,30 @@
+// deno-lint-ignore no-explicit-any
+export function not<T extends (...args: any) => boolean>(fn: T): T {
+  const proxy = new Proxy(fn, {
+    apply: (target, thisArg, argArray) => {
+      return !target.apply(thisArg, argArray);
+    },
+  });
+
+  return proxy;
+}
+
+export function replaceWith(newNode: Node, oldNode: Node): boolean {
+  return !!oldNode.parentNode?.replaceChild(newNode, oldNode);
+}
+
+export function remove(node: Node): boolean {
+  return !!node.parentNode?.removeChild(node);
+}
+
+export function removeAttributeNode(node: Attr): boolean {
+  if (node.ownerElement) {
+    node.ownerElement.removeAttributeNS(
+      node.namespaceURI,
+      node.localName,
+    );
+    return true;
+  }
+
+  return false;
+}
