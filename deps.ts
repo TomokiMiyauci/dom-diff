6d3d6d4075e9de20c999a16c0d9b70e0b9e259d5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-29 16:10:59 +0900

    chore(deps): add deps

---
+++ deps.ts
@@ -0,0 +1,5 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { distinct } from "https://deno.land/std@0.195.0/collections/distinct.ts";
+export { zip } from "https://deno.land/std@0.195.0/collections/zip.ts";
