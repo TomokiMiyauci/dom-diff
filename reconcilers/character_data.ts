b7135ca8585105b8e1a0aea3ac201f0145353d58
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-03 11:48:40 +0900

    feat(character_data): add character data reconciler

---
+++ reconcilers/character_data.ts
@@ -0,0 +1,46 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+/// <reference lib="dom" />
+
+import { type CharacterDataLike } from "../types.ts";
+
+export interface CharacterDataPatch {
+  action: "substitute";
+  from: string;
+  to: string;
+}
+
+export function* diffCharacterData(
+  oldNode: Node | CharacterDataLike,
+  newNode: Node | CharacterDataLike,
+): Generator<CharacterDataPatch> {
+  if (!("data" in oldNode && "data" in newNode)) return;
+  if (equalsData(oldNode, newNode)) return;
+
+  yield { action: "substitute", from: oldNode.data, to: newNode.data };
+}
+
+export function syncCharacterData(
+  node: Node | CharacterDataLike,
+  patch: CharacterDataPatch,
+): void {
+  if ("data" in node) {
+    node.data = patch.to;
+    return;
+  }
+
+  throw new Error("target node should have data property");
+}
+
+export class CharacterDataReconciler {
+  static diff = diffCharacterData;
+  static sync = syncCharacterData;
+}
+
+export function equalsData(
+  left: { data: unknown },
+  right: { data: unknown },
+): boolean {
+  return left.data === right.data;
+}
