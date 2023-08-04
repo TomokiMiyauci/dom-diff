39fdcb7bab2f7ec629025241f0fe5aa44261f0f5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-04 13:40:50 +0900

    feat(mod): export setup event listener function

--- mod.ts
+++ mod.ts
@@ -7,3 +7,4 @@ export { AttributeReconciler } from "./reconcilers/attribute.ts";
 export { MarkupReconciler } from "./reconcilers/markup.ts";
 export { EventHandlerReconciler } from "./reconcilers/event_handler.ts";
 export { EventListenerReconciler } from "./reconcilers/event_listener.ts";
+export { setup as setupEventListener } from "./utils/get_event_listeners/mod.ts";
