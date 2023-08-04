707349b637dbf2da7648b70b827fdd9017e7f54b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-04 13:02:23 +0900

    feat(mod): export reconciler modules

--- mod.ts
+++ mod.ts
@@ -1,5 +1,9 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { diff, diffChildren } from "./diff.ts";
-export { applyPatch } from "./patch.ts";
+export { Differ } from "./diff.ts";
+export { CharacterDataReconciler } from "./reconcilers/character_data.ts";
+export { AttributeReconciler } from "./reconcilers/attribute.ts";
+export { MarkupReconciler } from "./reconcilers/markup.ts";
+export { EventHandlerReconciler } from "./reconcilers/event_handler.ts";
+export { EventListenerReconciler } from "./reconcilers/event_listener.ts";
