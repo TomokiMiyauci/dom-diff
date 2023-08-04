80fe0147ae8a0460c59978122027e48b04d7987b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-04 16:06:55 +0900

    chore(data): change interface to object

--- reconcilers/data.ts
+++ reconcilers/data.ts
@@ -1,43 +1,49 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
-/// <reference lib="dom" />
+import { PatchType, type Reconciler, type SubstitutePatch } from "../types.ts";
 
-import { type CharacterDataLike } from "./types.ts";
-import { type SubstitutePatch } from "../types.ts";
+export type DataPatch = SubstitutePatch<unknown>;
 
-export type CharacterDataPatch = SubstitutePatch<string>;
+export function* diffData(
+  oldNode: object,
+  newNode: object,
+): Generator<DataPatch> {
+  if (
+    !("data" in oldNode && "data" in newNode) ||
+    oldNode.data === newNode.data
+  ) return;
 
-export function* diffCharacterData(
-  oldNode: Node | CharacterDataLike,
-  newNode: Node | CharacterDataLike,
-): Generator<CharacterDataPatch> {
-  if (!("data" in oldNode && "data" in newNode)) return;
-  if (equalsData(oldNode, newNode)) return;
-
-  yield { action: "substitute", from: oldNode.data, to: newNode.data };
+  yield { action: PatchType.Substitute, from: oldNode.data, to: newNode.data };
 }
 
-export function syncCharacterData(
-  node: Node | CharacterDataLike,
-  patch: CharacterDataPatch,
-): void {
-  if ("data" in node) {
-    node.data = patch.to;
-    return;
-  }
+export function syncData(node: object, patch: DataPatch): void {
+  if (!("data" in node)) throw new Error("node should have \`data\` property");
 
-  throw new Error("target node should have data property");
+  node.data = patch.to;
 }
 
-export class CharacterDataReconciler {
-  static diff = diffCharacterData;
-  static sync = syncCharacterData;
-}
-
-export function equalsData(
-  left: { data: unknown },
-  right: { data: unknown },
-): boolean {
-  return left.data === right.data;
+/** `data` property reconciler.
+ *
+ * @example
+ * ```ts
+ * import { DataReconciler } from "https://deno.land/x/dom_diff/reconcliers/data.ts";
+ * declare const data1: string;
+ * declare const data2: string;
+ * const oldNode = document.createTextNode(data1);
+ * const newNode = document.createTextNode(data2);
+ * const reconciler = new DataReconciler();
+ * const patches = reconciler.diff(oldNode, newNode);
+ *
+ * for (const patch of patches) {
+ *  reconciler.update(oldNode, patch);
+ * }
+ *
+ * assertEquals(oldNode.data, data2);
+ *
+ * ```
+ */
+export class DataReconciler implements Reconciler<DataPatch> {
+  diff = diffData;
+  update = syncData;
 }
