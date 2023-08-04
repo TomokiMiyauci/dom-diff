f8740bc01af3dcb592efdc4a8852e1d0a1750f67
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-04 15:06:43 +0900

    feat(mod): export related types

--- mod.ts
+++ mod.ts
@@ -8,3 +8,4 @@ export { MarkupReconciler } from "./reconcilers/markup.ts";
 export { EventHandlerReconciler } from "./reconcilers/event_handler.ts";
 export { EventListenerReconciler } from "./reconcilers/event_listener.ts";
 export { setup as setupEventListener } from "./utils/get_event_listeners/mod.ts";
+export { type Reconciler } from "./types.ts";
