9e855d66dad299988ce612a47fa20d3b0b261626
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-05 22:34:29 +0900

    docs(constants): add header license

--- utils/get_event_listeners/constants.ts
+++ utils/get_event_listeners/constants.ts
@@ -1,3 +1,9 @@
-import type { EventMapRegistry } from "./registry.ts";
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
 
-export const eventTargetRegistry = new WeakMap<object, EventMapRegistry>();
+import type { EventListenerMap } from "./map.ts";
+
+export const eventTargetRegistry = /*@__PURE__*/ new WeakMap<
+  object,
+  EventListenerMap
+>();
