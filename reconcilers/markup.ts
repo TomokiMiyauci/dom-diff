1d8870208f1bb09ecd1a39caa93f58385bf18faf
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-05 14:41:33 +0900

    docs(markup): add JSDoc comment

--- reconcilers/markup.ts
+++ reconcilers/markup.ts
@@ -5,51 +5,71 @@
 
 import { assoc, imap } from "../deps.ts";
 import { AttributePatch, diffAttribute, syncAttribute } from "./attribute.ts";
-import {
-  CharacterDataPatch,
-  diffCharacterData,
-  syncCharacterData,
-} from "./character_data.ts";
+import { DataPatch, diffData, syncData } from "./data.ts";
 import type { Reconciler } from "../types.ts";
 
-enum DataType {
+export const enum DataType {
   Attribute = "attribute",
-  CharacterData = "character-data",
+  Data = "data",
 }
 
-interface TypedCharacterDataPatch extends CharacterDataPatch {
-  type: DataType.CharacterData;
+interface TypedDataPatch extends DataPatch {
+  type: DataType.Data;
 }
 
 type TypedAttributePatch = AttributePatch & {
   type: DataType.Attribute;
 };
 
+/** Markup patch. */
 export type MarkupPatch =
   | TypedAttributePatch
-  | TypedCharacterDataPatch;
+  | TypedDataPatch;
 
-const injectTypeOfAttribute = assoc("type", DataType.Attribute);
-const injectTypeOfCharacterData = assoc("type", DataType.CharacterData);
+const injectTypeOfAttribute = /*@__PURE__*/ assoc("type", DataType.Attribute);
+const injectTypeOfCharacterData = /*@__PURE__*/ assoc("type", DataType.Data);
 
 export function* diffMarkup(
   oldNode: Node,
   newNode: Node,
-): IterableIterator<MarkupPatch> {
+): Generator<MarkupPatch, void, undefined> {
   yield* imap(diffAttribute(oldNode, newNode), injectTypeOfAttribute);
-  yield* imap(diffCharacterData(oldNode, newNode), injectTypeOfCharacterData);
+  yield* imap(diffData(oldNode, newNode), injectTypeOfCharacterData);
 }
 
 export function syncMarkup(node: Node, patch: MarkupPatch): void {
   switch (patch.type) {
     case DataType.Attribute:
       return syncAttribute(node, patch);
-    case DataType.CharacterData:
-      return syncCharacterData(node, patch);
+    case DataType.Data:
+      return syncData(node, patch);
   }
 }
 
+/** Markup reconciler.
+ * @example
+ * ```ts
+ * import { MarkupReconciler } from "https://deno.land/x/dom_diff/reconcilers/markup.ts";
+ * import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+ * const parser = new DOMParser();
+ * const oldNode = `<div></div>`;
+ * const newNode = `<div title="test"></div>`;
+ * const oldRoot = parser.parseFromString(oldNode, "text/html");
+ * const newRoot = parser.parseFromString(newNode, "text/html");
+ * const reconciler = new MarkupReconciler();
+ * const patches = reconciler.diff(
+ *   oldRoot.body.firstChild!,
+ *   newRoot.body.firstChild!,
+ * );
+ *
+ * for (const patch of patches) {
+ *   reconciler.update(oldRoot.body.firstChild!, patch);
+ * }
+ *
+ * assertEquals(oldRoot.body.innerHTML, newNode);
+ * ```
+ */
 export class MarkupReconciler implements Reconciler<MarkupPatch> {
   diff = diffMarkup;
-  sync = syncMarkup;
+  update = syncMarkup;
 }
