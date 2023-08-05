b438719dcd0fe3e62efba729d5a0b115bec4fc42
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-06 00:36:29 +0900

    perf(event_listener): improve filtering performance to use composite symbol
    to be O(n)

--- reconcilers/event_listener.ts
+++ reconcilers/event_listener.ts
@@ -4,71 +4,77 @@
 /// <reference lib="dom" />
 
 import { distinct } from "../deps.ts";
-import type { AdditionPatch, DeletionPatch, Reconciler } from "../types.ts";
 import {
-  type EventInfo,
-  getEventListeners,
+  type AdditionPatch,
+  type DeletionPatch,
+  PatchType,
+  type Reconciler,
+} from "../types.ts";
+import {
+  type EventListeners,
+  type GetEventListeners,
+  type Listener,
 } from "../utils/get_event_listeners/mod.ts";
+import {
+  compositeSymbol,
+} from "https://deno.land/x/composite_key@1.0.0/mod.ts";
+
+export {
+  setup as setupEventListeners,
+} from "../utils/get_event_listeners/mod.ts";
+export type { EventListeners, GetEventListeners };
 
 export type EventListenerPatch =
-  | AdditionPatch<EventInfo>
-  | DeletionPatch<EventInfo>;
+  | AdditionPatch<Listener>
+  | DeletionPatch<Listener>;
 
 export function* diffEventLister(
-  oldNode: Node,
-  newNode: Node,
+  oldNode: object,
+  newNode: object,
+  getEventListeners: GetEventListeners,
 ): Generator<EventListenerPatch> {
-  const oldListeners = new Map<string, EventInfo[]>(
-    Object.entries(getEventListeners(oldNode)),
+  yield* generatePatches(
+    getEventListeners(oldNode),
+    getEventListeners(newNode),
   );
-  const newListeners = new Map<string, EventInfo[]>(
-    Object.entries(getEventListeners(newNode)),
-  );
+}
 
-  const allEvents = distinct([...oldListeners.keys(), ...newListeners.keys()]);
+function listener2Entry(listener: Listener): [key: symbol, listener: Listener] {
+  return [
+    compositeSymbol(listener.type, listener.listener, listener.useCapture),
+    listener,
+  ];
+}
 
-  for (const event of allEvents) {
-    if (!oldListeners.has(event)) {
-      const events = newListeners.get(event)!;
+function listeners2Entries(
+  listeners: readonly Listener[],
+): [key: symbol, listener: Listener][] {
+  return listeners.map(listener2Entry);
+}
 
-      for (const event of events) {
-        yield { ...event, action: "add" };
-      }
+export function* generatePatches(
+  oldListeners: EventListeners,
+  newListeners: EventListeners,
+): Generator<EventListenerPatch> {
+  const oldEntries = Object.values(oldListeners).flatMap(listeners2Entries);
+  const newEntries = Object.values(newListeners).flatMap(listeners2Entries);
+  const oldMaps = new Map<symbol, Listener>(oldEntries);
+  const newMaps = new Map<symbol, Listener>(newEntries);
+  const allKeys = distinct([...oldMaps.keys(), ...newMaps.keys()]);
 
-      return;
+  for (const key of allKeys) {
+    if (!oldMaps.has(key)) {
+      const listener = newMaps.get(key)!;
+
+      yield { ...listener, action: PatchType.Add };
+      continue;
     }
 
-    if (!newListeners.has(event)) {
-      const events = oldListeners.get(event)!;
+    if (!newMaps.has(key)) {
+      const listener = oldMaps.get(key)!;
 
-      for (const event of events) {
-        yield { ...event, action: "delete" };
-      }
-
-      return;
-    }
-
-    const lefts = oldListeners.get(event)!;
-    const rights = newListeners.get(event)!;
-
-    const commons = intersectBy(
-      lefts,
-      rights,
-      equalsListenerObject,
-    );
-    const leftsOnly = lefts.filter((left) =>
-      commons.find((right) => !equalsListenerObject(left, right))
-    );
-    const rightsOnly = rights.filter((right) =>
-      commons.find((left) => !equalsListenerObject(right, left))
-    );
-
-    for (const listener of leftsOnly) {
-      yield { action: "delete", ...listener };
-    }
-
-    for (const listener of rightsOnly) {
-      yield { action: "add", ...listener };
+      yield { ...listener, action: PatchType.Delete };
+      continue;
     }
   }
 }
@@ -80,38 +86,57 @@ export function syncEventListener(
   const { type, listener, action, ...options } = patch;
 
   switch (action) {
-    case "add": {
-      target.addEventListener(type, listener, options);
+    case PatchType.Add: {
+      const { useCapture: capture, once, passive } = options;
+      target.addEventListener(type, listener, { capture, once, passive });
       break;
     }
-    case "delete": {
-      target.removeEventListener(type, listener, { capture: options.capture });
+
+    case PatchType.Delete: {
+      target.removeEventListener(type, listener, {
+        capture: options.useCapture,
+      });
       break;
     }
   }
 }
 
-function equalsListenerObject(left: EventInfo, right: EventInfo): boolean {
-  return left.listener === right.listener && left.capture === right.capture;
-}
+/** Event listener reconciler.
+ *
+ * @example
+ * ```ts
+ * import { EventListenerReconciler, setupEventListeners } from "https://deno.land/x/dom_diff/reconcilers/event_listener.ts";
+ * import { assertSpyCalls, spy } from "https://deno.land/std/testing/mock.ts";
+ *
+ * const getEventListeners = setupEventListeners();
+ * const reconciler = new EventListenerReconciler(getEventListeners);
+ * const oldNode = document.createElement("button");
+ * const newNode = document.createElement("button");
+ * const listener = spy(() => {});
+ * newNode.addEventListener("click", listener);
+ *
+ * const patches = reconciler.diff(oldNode, newNode);
+ * for (const patch of patches) reconciler.update(oldNode, patch);
+ *
+ * oldNode.dispatchEvent(new MouseEvent("click"));
+ * assertSpyCalls(listener, 1);
+ * ```
+ */
+export class EventListenerReconciler implements Reconciler<EventListenerPatch> {
+  /**
+   * @example
+   * ```ts
+   * import { setup } from "https://deno.land/x/dom_diff/utils/get_event_listeners/mod.ts";
+   * import { EventListenerReconciler } from "https://deno.land/x/dom_diff/reconcilers/event_listener.ts";
+   *
+   * const getEventListeners = setup();
+   * const reconciler = new EventListenerReconciler(getEventListeners);
+   * ```
+   */
+  constructor(public getEventListeners: GetEventListeners) {}
 
-function intersectBy<T>(
-  lefts: readonly T[],
-  rights: readonly T[],
-  equals: (left: T, right: T) => boolean,
-): T[] {
-  const result = lefts.reduce((acc, left) => {
-    const matched = rights.find((right) => equals(left, right));
-
-    if (matched) acc.push(left);
-
-    return acc;
-  }, [] as T[]);
-
-  return result;
-}
-
-export class EventListenerReconciler implements Reconciler<EventInfo> {
-  diff = diffEventLister;
-  sync = syncEventListener;
+  diff(oldNode: object, newNode: object): Generator<EventListenerPatch> {
+    return diffEventLister(oldNode, newNode, this.getEventListeners);
+  }
+  update = syncEventListener;
 }
