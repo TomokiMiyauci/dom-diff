09f4b6a2e21440c84ae0eeacaa2cf69e4724430b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-05 22:32:11 +0900

    test(setup): add test case

--- utils/get_event_listeners/setup.ts
+++ utils/get_event_listeners/setup.ts
@@ -1,6 +1,10 @@
-import { EventMapRegistry } from "./registry.ts";
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import { EventListenerMap } from "./map.ts";
 import { normalizeOptions } from "./utils.ts";
 import { eventTargetRegistry } from "./constants.ts";
+import type { EventListeners, Listener } from "./types.ts";
 
 /** Setup options. */
 export interface SetupOptions {
@@ -15,45 +19,98 @@ export interface SetupOptions {
   removeEventListener?: EventTarget["removeEventListener"];
 }
 
-export function setup(options?: SetupOptions): void {
+export interface GetEventListeners {
+  (target: EventTarget): EventListeners;
+}
+
+/** Setup event listener monitoring.
+ * Performs a side effect and changes the prototype of `EventTarget`.
+ */
+export function setup(options?: SetupOptions): GetEventListeners {
   const addEventListener = options?.addEventListener ??
     EventTarget.prototype.addEventListener;
   const removeEventListener = options?.removeEventListener ??
     EventTarget.prototype.removeEventListener;
 
   const $addEventListener = new Proxy(addEventListener, {
-    apply: (target, thisArg, argArray) => {
-      const [type, listener, options] = argArray;
-
-      if (eventTargetRegistry.has(thisArg)) {
-        const map = eventTargetRegistry.get(thisArg)!;
-
-        map.set(type, listener, options);
-      } else {
-        const map = new EventMapRegistry();
-        map.set(type, listener, options);
-
-        eventTargetRegistry.set(thisArg, map);
-      }
-
-      return target.apply(thisArg, argArray as never);
-    },
+    apply: (target, thisArg, argArray) =>
+      handleAddEventListener(
+        target,
+        thisArg,
+        argArray as Parameters<typeof EventTarget.prototype.addEventListener>,
+        eventTargetRegistry,
+      ),
   });
 
   const $removeEventListener = new Proxy(removeEventListener, {
-    apply: (target, thisArg, argArray) => {
-      if (eventTargetRegistry.has(thisArg)) {
-        const [type, listener, options] = argArray;
-        const { capture } = normalizeOptions(options);
-        const eventRegistry = eventTargetRegistry.get(thisArg)!;
-
-        eventRegistry.delete(type, listener, capture);
-      }
-
-      return target.apply(thisArg, argArray as never);
-    },
+    apply: (target, thisArg, argArray) =>
+      handlerRemoveEventListener(
+        target,
+        thisArg,
+        argArray as Parameters<
+          typeof EventTarget.prototype.removeEventListener
+        >,
+        eventTargetRegistry,
+      ),
   });
 
   EventTarget.prototype.addEventListener = $addEventListener;
   EventTarget.prototype.removeEventListener = $removeEventListener;
+
+  return getEventListeners;
+}
+
+export function handleAddEventListener<T extends object, R>(
+  target: (
+    this: T,
+    ...args: Parameters<typeof EventTarget.prototype.addEventListener>
+  ) => R,
+  thisArg: T,
+  argArray: Parameters<typeof EventTarget.prototype.addEventListener>,
+  registry: WeakMap<T, EventListenerMap>,
+): R {
+  const [type, listener, options] = argArray;
+  const $listener: Listener = { type, listener, ...normalizeOptions(options) };
+
+  if (registry.has(thisArg)) {
+    const map = registry.get(thisArg)!;
+
+    map.set($listener);
+  } else {
+    const map = new EventListenerMap([$listener]);
+
+    registry.set(thisArg, map);
+  }
+
+  return target.apply(thisArg, argArray);
+}
+
+export function handlerRemoveEventListener<T extends object, R>(
+  target: (
+    this: T,
+    ...args: Parameters<typeof EventTarget.prototype.removeEventListener>
+  ) => R,
+  thisArg: T,
+  argArray: Parameters<typeof EventTarget.prototype.removeEventListener>,
+  registry: WeakMap<T, EventListenerMap>,
+): R {
+  if (registry.has(thisArg)) {
+    const eventRegistry = registry.get(thisArg)!;
+    const [type, listener, options] = argArray;
+
+    eventRegistry.delete({ type, listener, ...normalizeOptions(options) });
+  }
+
+  return target.apply(thisArg, argArray);
+}
+
+/** Get {@linkcode EventListeners}.
+ * Before this can be done, {@linkcode setup} must be performed.
+ */
+export function getEventListeners(target: EventTarget): EventListeners {
+  const eventMap = eventTargetRegistry.get(target);
+
+  if (!eventMap) return {};
+
+  return Object.fromEntries(eventMap);
 }
