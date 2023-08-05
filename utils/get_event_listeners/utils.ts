46668856f48419c4ecdb7f1ab330c4226cadf89d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-05 16:05:52 +0900

    test(utils): add test case

--- utils/get_event_listeners/utils.ts
+++ utils/get_event_listeners/utils.ts
@@ -1,7 +1,11 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import type { NormalizedAddEventListenerOptions } from "./types.ts";
 
+/** Normalize event listener options. */
 export function normalizeOptions(
-  options: boolean | AddEventListenerOptions | undefined,
+  options?: boolean | Omit<AddEventListenerOptions, "signal"> | undefined,
 ): NormalizedAddEventListenerOptions {
   if (typeof options === "boolean" || typeof options === "undefined") {
     return {
